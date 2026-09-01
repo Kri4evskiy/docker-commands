dd006d308c4b01791ce4b88645eeaede0e0478d1
Stanislav <kri4evskiy@gmail.com>
2026-09-01 13:36:24 +0300

    feat: initialize project scaffolding with docker-compose orchestration and basic frontend/api configurations

--- time-app/frontend/src/components/Time.vue
+++ time-app/frontend/src/components/Time.vue
@@ -30,20 +30,11 @@ export default {
   <div class="greetings">
     <h1 class="green">{{ currentTime }}</h1>
     <h3>Нажмите кнопку для сохранения в базе данных</h3>
-    <button @click="saveTime">Сохранить время</button>
-    <h3 v-if="savedTimes.length && showSavedTimes">
-      Ранее сохраненные времена:
-    </h3>
-    <div
-      v-if="showSavedTimes"
-      class="deleted-items"
-      v-for="savedTime in savedTimes"
-      :key="savedTime.id"
-    >
+    <button @click="saveTime">Сохранить текущее время</button>
+    <h3 v-if="savedTimes.length && showSavedTimes">Ранее сохраненные времена:</h3>
+    <div v-if="showSavedTimes" class="deleted-items" v-for="savedTime in savedTimes" :key="savedTime.id">
       <div class="deleted-item">{{ savedTime.time }}</div>
-      <button class="btn-sm bg-red" @click="() => deleteTime(savedTime.id)">
-        Удалить
-      </button>
+      <button class="btn-sm bg-red" @click="() => deleteTime(savedTime.id)">Удалить</button>
     </div>
   </div>
 </template>
